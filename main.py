0c862795e5a5815e586c43e0e724da443351f3a0
John Doe <johndoe@example.com>
2024-12-11 22:16:33 +0100

    some dpg advancements

--- main.py
+++ main.py
@@ -22,31 +22,6 @@ def init_players(players_amount:int, trains_draw, tickets_draw):
     return players
 
 
-def init_gameplay_elements():
-    # map
-    map_img = pg.image.load(MEDIAS_REPERTORY + MAP_IMG)
-    x_map, _ = map_img.get_size()
-    window.blit(map_img, ((x_window/2-x_map/2),0))
-    
-    # train cards
-    train_cards = {}
-    train_cards["yellow"] = pg.image.load(TRAINS_REPERTORY + "yellow.png")
-    train_cards["white"] = pg.image.load(TRAINS_REPERTORY + "white.png")
-    train_cards["red"] = pg.image.load(TRAINS_REPERTORY + "red.png")
-    train_cards["pink"] = pg.image.load(TRAINS_REPERTORY + "pink.png")
-    train_cards["orange"] = pg.image.load(TRAINS_REPERTORY + "orange.png")
-    train_cards["green"] = pg.image.load(TRAINS_REPERTORY + "green.png")
-    train_cards["blue"] = pg.image.load(TRAINS_REPERTORY + "blue.png")
-    train_cards["black"] = pg.image.load(TRAINS_REPERTORY + "black.png")
-    train_cards["locomotive"] = pg.image.load(TRAINS_REPERTORY + "locomotive.png")
-    
-    x_trains_cards, y_trains_cards = train_cards["yellow"].get_size()
-    x = 0
-    for _, surface in train_cards.items():
-        window.blit(surface, (x, y_window-y_trains_cards))
-        x += x_trains_cards
-
-
 def count_train_cards_color(player) -> dict:
     """Returns a dictionnary with as key the color of the card
     and as value the amount of cards of this color
@@ -62,12 +37,8 @@ def count_train_cards_color(player) -> dict:
 
 
 TTR_ICON = "icon_ticketToRide.png"
-MAP_IMG = "map.jpg"
-TRAINS_REPERTORY = MEDIAS_REPERTORY + "train_cards/"
-
-#########################
-# Setting up the window #
-
+MAP_IMAGE = "map.jpg"
+TRAINS_IMAGES = MEDIAS_REPERTORY + "train_cards/"
 
 
 #######################
@@ -78,30 +49,56 @@ board = Board()
 # Setting up players #
 players = init_players(2, board.trains_draw.draw, board.tickets_draw.draw)
 
+############
+# Graphics #
+dpg.create_context()
+dpg.create_viewport(title='Ticket To Ride : Europe')
 
-# display player's train cards
+
+# Player's Deck
 player_trains_cards = count_train_cards_color(players[0])
-
-for color, amount in player_trains_cards.items():
-    pass
-    
-# display player's ticket cards
 player_tickets = players[0].tickets_deck
-for ticket in player_tickets:
-    pass
-
-# display player's trains
 player_trains = players[0].trains
-
-# display player's stations
 player_stations = players[0].stations
 
-# display offer
-for card in board.trains_draw.offer.deck:
-    pass
+with dpg.window(label="Your trains deck", autosize=True):
+    # display player's train cards
+    for color, amount in player_trains_cards.items():
+        dpg.add_text(f"{color} : {amount}")
+
+with dpg.window(label="Your tickets deck", autosize=True):
+    # display player's ticket cards
+    for ticket in player_tickets:
+        dpg.add_text(f"{ticket.get_city_a()} -->  {ticket.get_city_b()} : {ticket.get_value()}")
+
+with dpg.window(label="Your pawn deck", autosize=True):
+    # display player's trains
+    dpg.add_text("trains : " + str(player_trains))    
+    # display player's stations
+    dpg.add_text("stations : " + str(player_stations))
+
+with dpg.window(label="Trains offer", autosize=True):
+    # display trains offer
+    dpg.add_button(label= "random")
+    for card in board.trains_draw.offer.deck:
+        dpg.add_button(label= card.get_color())
+        
+with dpg.window(label="Tickets offer", autosize=True):
+    # display ticket offer
+    dpg.add_button(label= "3 random cards")
+        
+
+
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.toggle_viewport_fullscreen()
+
+dpg.start_dearpygui()
+dpg.destroy_context()
+
+
 
 #############
 # Game loop #
-
-
 
